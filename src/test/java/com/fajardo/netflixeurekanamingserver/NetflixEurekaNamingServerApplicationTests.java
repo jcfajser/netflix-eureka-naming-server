c892375d134c788fd052fddf6cbf0a1be31e3c73
Juan Carlos Fajardo <juan.fajardo@softtek.com>
2021-11-23 14:42:26 -0600

    Initial commit

---
+++ src/test/java/com/fajardo/netflixeurekanamingserver/NetflixEurekaNamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fajardo.netflixeurekanamingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixEurekaNamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
